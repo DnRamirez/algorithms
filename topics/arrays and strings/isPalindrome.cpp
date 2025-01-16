e449e1e3e96c05defb42419c001314a45dcd3621
Danster23 <ramirezdanny02@gmail.com>
2025-01-15 21:00:18 -0600

    comments

--- topics/arrays and strings/isPalindrome.cpp
+++ topics/arrays and strings/isPalindrome.cpp
@@ -3,9 +3,9 @@ using namespace std;
  
 /*  
     Approach: Two pointers, one at beginning of string and the other at the end.
-    Complexity: O(n)
-    Analysis: The pointers start at n away from each other. Each iteration brings them at least one step closer. 
-              We will never have more than O(n) iterations for the while loop.
+    Time complexity: O(n)
+    Space complexity: O(1) (initializing two integer variables)
+    Analysis: The pointers start at n away from each other. Each iteration brings them at least one step closer. We will never have more than O(n) iterations for the while loop.
 */
 
 bool isPalindrome(string s) { 
