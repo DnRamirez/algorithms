88f0a615c2bc590bd3febb03ead091fc450c5f45
Danster23 <ramirezdanny02@gmail.com>
2025-03-06 19:47:16 -0600

    intersection of multiple arrays

---
+++ topics/hashmap/intersectionMultipleArrs.cpp
@@ -0,0 +1,37 @@
+#include <iostream> 
+#include <unordered_map>
+using namespace std; 
+
+vector<int> sortedArr(vector<vector<int>> nums) { 
+    unordered_map<int, int> elemFreq; 
+    vector<int> ans; 
+    int n = nums.size(); 
+    for (vector<int> arr : nums) { 
+        for (int x : arr) { 
+            elemFreq[x]++; 
+        }
+    }
+
+    for(auto i : elemFreq) { 
+        if (i.second == n) {
+            ans.push_back(i.first); 
+        }
+    }
+
+    return ans; 
+    
+}
+
+int main() {
+    
+    vector<vector<int>> test = {{3,1,2,4,5},{1,2,3,4},{3,4,5,6}}; 
+
+    vector<int> answer = sortedArr(test); 
+
+    for (int i = 0; i < answer.size(); i++) { 
+        cout << answer[i] << ", "; 
+    }
+
+
+    return 0; 
+}
