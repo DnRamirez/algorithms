30219915e0c6621bc6d984a7143bff3f69db109a
Danster23 <ramirezdanny02@gmail.com>
2025-03-06 22:03:38 -0600

    equal occurence

---
+++ topics/hashmap/equalOccurence.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <unordered_map> 
+#include <unordered_set>
+using namespace std; 
+
+bool isEqual(string s) { 
+    unordered_map<char, int> counts; 
+    for (int i = 0; i < s.size(); i++) { 
+        counts[s[i]]++; 
+    }
+
+    unordered_set<int> frequencies; 
+    for (auto it : counts) { 
+        frequencies.insert(it.second); 
+    }
+
+    return frequencies.size() == 1; 
+
+}
+
+int main() { 
+    string s = "abacbc"; 
+    if (isEqual(s)) { 
+        cout << "Elements have equal frequencies" << endl; 
+    }
+
+    return 0; 
+}
