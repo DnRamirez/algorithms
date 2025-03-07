d12d1002f3fa742c8b7f74578487aa02326b28e5
Danster23 <ramirezdanny02@gmail.com>
2025-03-06 18:54:26 -0600

    comment changes

--- topics/hashmap/countingElements.cpp
+++ topics/hashmap/countingElements.cpp
@@ -1,3 +1,6 @@
+/* Problem: Given an integer array arr, count how many elements x there are, such that x + 1 is also in arr. If there are duplicates in arr, count them separately.
+
+*/
 #include <iostream> 
 #include <vector> 
 #include <unordered_set> 
